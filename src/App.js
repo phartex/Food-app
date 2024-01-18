09ab0cb4fdca23ba39e7bcce0266b63b7bf1150a
fateru tobi <fateruvictor96@gmail.com>
2024-01-18 01:26:10 +0100

    finished responsiveness

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import HeadlineCards from "./component/HeadlineCards";
+import Food from "./component/Food"
+import Category from "./component/Category";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="px-4 md:px-20">
+        <Navbar/>
+        <Hero/>
+        <HeadlineCards/>
+        <Food/>
+        <Category/>
+      </div>
   );
 }
 
